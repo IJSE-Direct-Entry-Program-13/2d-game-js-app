3228be3aae492e9595771c3cb137e468b2dc11d3
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-03-08 22:29:25 +0530

    Add README.md files

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,8 @@ $("body").append(character);
 
 let dy = 0;
 let dx = 0;
-let g = 4 ;
+let g = 4;
+let x = 0;
 
 setInterval(() => {
     character.css('left', `${character.position().left + dx}px`);
@@ -18,32 +19,43 @@ setInterval(() => {
         dy = 0;
         character.css('top', `${innerHeight - character.outerHeight()}px`)
     }
+    if (dx === 0){
+        character.css('background-image', `url('./public/img/idle/Idle (${x++}).png')`)
+        if (x === 16) x = 1;
+    }else{
+        character.css('background-image', `url('./public/img/idle/Idle (${x++}).png')`)
+
+    }
 }, 50);
 
-$(window).on('keydown', (e)=>{
-    switch (e.code){
+$(window).on('keydown', (e) => {
+    switch (e.code) {
         case "ArrowRight":
+            if (dx === 0) x = 1;
             dx = 10;
             break;
         case "ArrowLeft":
             dx = -10;
+            x=1;
             break;
         case "Space":
-            if (dy === 0){
+            if (dy === 0) {
                 // dy = 20;
-                character.css('top', `${innerHeight - 350 }px`);
+                character.css('top', `${innerHeight - 350}px`);
             }
     }
 });
 
-$(window).on('keyup', (e)=>{
-    switch (e.code){
-        case "ArrowRight": case "ArrowLeft":
+$(window).on('keyup', (e) => {
+    switch (e.code) {
+        case "ArrowRight":
+        case "ArrowLeft":
             dx = 0;
+            x=1;
             break;
         case "Space":
-            if (dy === 0){
-                character.css('top', `${innerHeight - 350 }px`);
+            if (dy === 0) {
+                character.css('top', `${innerHeight - 350}px`);
             }
     }
 });
